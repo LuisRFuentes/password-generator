81cc33adc6559318aa1564272baaa527e595ec70
LuisRFuentes <luisrf0540@gmail.com>
2020-12-20 04:23:22 -0400

    Change variables to constans

--- password-generator.py
+++ password-generator.py
@@ -1,20 +1,24 @@
+# Password Generator
+# Project made for the course "Python Basic Course" in Platzi
+# Made by: LuisRFuentes
+
 import random
 
 
 def generate_password(use_mayus, use_minus, use_numbers, use_simbols, pass_length):
-    mayus = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-    minus = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-    numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    simbols = ('/', '*', '-', '_', '@', '&', '%', '!')
+    MAYUS = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    MINUS = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    NUMBERS = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    SIMBOLS = ('/', '*', '-', '_', '@', '&', '%', '!')
     combination = []
     if use_mayus:
-        combination += mayus
+        combination += MAYUS
     if use_minus:
-        combination += minus
+        combination += MINUS
     if use_numbers:
-        combination += numbers
+        combination += NUMBERS
     if use_simbols:
-        combination += simbols
+        combination += SIMBOLS
     password = ""
     i = 0
     while i < pass_length:
